712fbd0bcf3e299b545f5aa55b68d064a1092fac
zfkj4090 <zfkj4090@DESKTOP-GJ77VA6>
2025-09-06 16:48:34 +0800

    增加启动时更新配置文件

--- ZipperScanner/src/main.cpp
+++ ZipperScanner/src/main.cpp
@@ -4,11 +4,13 @@
 #include <QtWidgets/QApplication>
 #include "rqw_RunEnvCheck.hpp"
 #include "Utilty.hpp"
+#include "GlobalStruct.hpp"
+#include "oso_func.hpp"
 int main(int argc, char* argv[])
 {
 	QApplication a(argc, argv);
 
-	ZipperScanner w;
+#pragma region running software check
 	// 运行前检查
 	if (!rw::rqw::RunEnvCheck::isSingleInstance("WetPapers.exe"))
 	{
@@ -30,6 +32,134 @@ int main(int argc, char* argv[])
 		QDir dir;
 		dir.mkpath(globalPath.configRootPath);
 	}
+#pragma endregion
+
+#pragma region check config file if format is error
+	rw::oso::StorageContext storageContext(rw::oso::StorageType::Xml);
+
+	{
+		std::shared_ptr<rw::oso::ObjectStoreAssembly> loadData = nullptr;
+		auto path = globalPath.generalConfigPath;
+		try
+		{
+			if (QFile::exists(path)) {
+				loadData = storageContext.loadSafe(path.toStdString());
+				if (!loadData)
+				{
+					QFile::remove(path);
+				}
+
+				cdm::GeneralConfig config = *loadData;
+			}
+		}
+		catch (std::runtime_error& e)
+		{
+			rw::oso::ObjectStoreAssembly newAssembly = cdm::GeneralConfig();
+			if (loadData)
+			{
+				rw::oso::AssemblyMergeTool::Merge(newAssembly, *loadData);
+				auto isSuccess = storageContext.saveSafe(newAssembly, path.toStdString());
+				if (isSuccess)
+				{
+					qDebug() << "GeneralConfig.xml update success";
+				}
+			}
+		}
+	}
+
+	{
+		std::shared_ptr<rw::oso::ObjectStoreAssembly> loadData = nullptr;
+		auto path = globalPath.scoreConfigPath;
+		try
+		{
+			if (QFile::exists(path)) {
+				loadData = storageContext.loadSafe(path.toStdString());
+				if (!loadData)
+				{
+					QFile::remove(path);
+				}
+
+				cdm::ScoreConfig config = *loadData;
+			}
+		}
+		catch (std::runtime_error& e)
+		{
+			rw::oso::ObjectStoreAssembly newAssembly = cdm::ScoreConfig();
+			if (loadData)
+			{
+				rw::oso::AssemblyMergeTool::Merge(newAssembly, *loadData);
+				auto isSuccess = storageContext.saveSafe(newAssembly, path.toStdString());
+				if (isSuccess)
+				{
+					qDebug() << "ScoreConfig.xml update success";
+				}
+			}
+		}
+	}
+
+	{
+		std::shared_ptr<rw::oso::ObjectStoreAssembly> loadData = nullptr;
+		auto path = globalPath.setConfigPath;
+		try
+		{
+			if (QFile::exists(path)) {
+				loadData = storageContext.loadSafe(path.toStdString());
+				if (!loadData)
+				{
+					QFile::remove(path);
+				}
+
+				cdm::SetConfig config = *loadData;
+			}
+		}
+		catch (std::runtime_error& e)
+		{
+			rw::oso::ObjectStoreAssembly newAssembly = cdm::SetConfig();
+			if (loadData)
+			{
+				rw::oso::AssemblyMergeTool::Merge(newAssembly, *loadData);
+				auto isSuccess = storageContext.saveSafe(newAssembly, path.toStdString());
+				if (isSuccess)
+				{
+					qDebug() << "SetConfig.xml update success";
+				}
+			}
+		}
+	}
+
+	{
+		std::shared_ptr<rw::oso::ObjectStoreAssembly> loadData = nullptr;
+		auto path = globalPath.dlgExposureTimeSetFilePath;
+		try
+		{
+			if (QFile::exists(path)) {
+				loadData = storageContext.loadSafe(path.toStdString());
+				if (!loadData)
+				{
+					QFile::remove(path);
+				}
+
+				cdm::ZipperScannerDlgExposureTimeSet config = *loadData;
+			}
+		}
+		catch (std::runtime_error& e)
+		{
+			rw::oso::ObjectStoreAssembly newAssembly = cdm::ZipperScannerDlgExposureTimeSet();
+			if (loadData)
+			{
+				rw::oso::AssemblyMergeTool::Merge(newAssembly, *loadData);
+				auto isSuccess = storageContext.saveSafe(newAssembly, path.toStdString());
+				if (isSuccess)
+				{
+					qDebug() << "ZipperScannerDlgExposureTimeSetConfig.xml update success";
+				}
+			}
+		}
+	}
+#pragma endregion
+
+	ZipperScanner w;
+	
 #ifdef NDEBUG
 	w.showFullScreen();
 #else
